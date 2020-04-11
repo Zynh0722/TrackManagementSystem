f0d2a3cbbd2e4858bc0d7d116299d7d801f52158
Zynh0722 <Zynh0722@gmail.com>
2020-04-11 01:23:33 -0700

    WHOLE BUNCH
    Don't mind the salmon

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow } = require('electron');
-
-require('electron-drag-drop');
+const path = require('path')
 
 function createWindow () {
     // Create the browser window.
@@ -8,6 +7,7 @@ function createWindow () {
         width: 800,
         height: 600,
         webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
             devTools: false,
         },
@@ -30,17 +30,14 @@ app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    if (process.platform !== 'darwin') app.quit();
+
 });
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 // In this file you can include the rest of your app's specific main process
---
+++ renderer.js
@@ -0,0 +1,48 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for (const empty of  empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => this.className = 'invisible', 0);
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'empty';
+}
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+}
+
+
