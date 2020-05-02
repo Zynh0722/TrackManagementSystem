834764bd98bcb793a64ad376f1e5cfacfae909a2
Ethan Hansen <1ethanhansen@gmail.com>
2020-05-01 17:21:10 -0700

    Reorganize test objects

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const test = require('ava')
 
 const {Vehicle, Driver} = require('./src/classes');
 
+let driver_test = new Driver("Marty McFly", "9303 Lyon Drive", "Hill Valley", "CA", 95420, 9168423138, "marty.mcfly@protonmail.com");
+let vehicle_test = new Vehicle("DeLorean", 1985, "Space grey", "Flux capacitor", "Timey Wimey", "Amblin Entertainment", "Time Travelers Anonymous")
+
+
 test('foo', t => {
     t.pass();
 });
@@ -12,11 +16,9 @@ test('bar', async t => {
 });
 
 test('driver_sanity', t => {
-    let driver_test = new Driver("Marty McFly", "9303 Lyon Drive", "Hill Valley", "CA", 95420, 9168423138, "marty.mcfly@protonmail.com");
     t.assert(driver_test.zip_code === 95420 && driver_test.state === "CA")
 });
 
 test('vehicle_sanity', t => {
-    let vehicle_test = new Vehicle("DeLorean", 1985, "Space grey", "Flux capacitor", "Timey Wimey", "Amblin Entertainment", "Time Travelers Anonymous")
     t.assert(vehicle_test.nomo === "Timey Wimey" && vehicle_test.engine === "Flux capacitor")
 });
